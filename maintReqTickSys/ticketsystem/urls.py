1055d0140649ff1ef5d4ebf3499adce9b8c52b32
Dalton Corriher <dmangames1941@gmail.com>
2025-03-06 09:41:12 -0500

    Very Basic Wireframe
    Added webpages for a home page, login page, user dashboard and admin dashboard. Updated urls.py and views.py. Added WIP login system.

--- maintReqTickSys/ticketsystem/urls.py
+++ maintReqTickSys/ticketsystem/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import home, user_login, user_logout, user_dashboard, admin_dashboard
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('', home, name='home'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('user-dashboard/', user_dashboard, name='user_dashboard'),
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
 ]
