9d8a5d072bbfb14484e3c5321707df3d78589aaa
JSkyeV <swtorjsv@icloud.com>
2025-04-16 17:05:50 -0400

    Revert "💌 Integrated HTML email system with new utils and templates"
    This reverts commit c7703e50dc68c3ae7df8be342605b9b0c22347a6.

--- maintReqTickSys/ticketsystem/apps.py
+++ maintReqTickSys/ticketsystem/apps.py
@@ -1,8 +1,7 @@
 from django.apps import AppConfig
 
+
 class TicketsystemConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'ticketsystem'
 
-    def ready(self):
-        import ticketsystem.signals  # 👈 this line loads signals!
